7bc86760ae8b07e4354b5e352bc5e292cd569ea5
sreyas <sreyas1045@gmail.com>
2024-10-03 23:35:02 +0530

    added postfix evaluation

--- C-C++/Stack/expressionEvaluation.c
+++ C-C++/Stack/expressionEvaluation.c
@@ -34,7 +34,7 @@ int pop()
 {
     if (isEmpty())
     {
-        printf("Stack Underflow!\n");
+
         return 0;
     }
     else
@@ -42,10 +42,59 @@ int pop()
         return stack[top--];
     }
 }
-int evaluate(char postfix[]){
-    int i =0 ;
-    while(postfix[i] != '\0')
+int evaluate(char postfix[])
+{
+    int i = 0, result, num, operand1, operand2;
+
+    while (postfix[i] != '\0')
     {
-        
+        if (postfix[i] >= '0' && postfix[i] <= '9')
+        {
+            num = postfix[i] - '0';
+            push(num);
+        }
+        else
+        {
+            operand1 = pop();
+            operand2 = pop();
+
+            switch (postfix[i])
+            {
+            case '+':
+                result = operand2 + operand1;
+                break;
+            case '-':
+                result = operand2 - operand1;
+                break;
+            case '*':
+                result = operand2 * operand1;
+                break;
+            case '/':
+                if (operand1 == 0)
+                {
+                    printf("Error: Division by zero!\n");
+                    return 0;
+                }
+                result = operand2 / operand1;
+                break;
+            case '^':
+                result = 1;
+                for (int j = 0; j < operand1; j++)
+                {
+                    result *= operand2;
+                }
+                break;
+            }
+            push(result);
+        }
+        i++;
     }
+    printf("Result : %d", result);
+}
+int main()
+{
+    char expression[20];
+    printf("Enter the expression : ");
+    fgets(expression, 20, stdin);
+    evaluate(expression);
 }
